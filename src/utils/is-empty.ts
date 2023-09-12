28d1ebba14b5345aa1f9a2b84ae75463af4ad75a
hwinkr <dnddl8280@naver.com>
2023-09-12 16:18:28 +0900

    Feat: 유틸 함수 구현

---
+++ src/utils/is-empty.ts
@@ -0,0 +1,5 @@
+const isEmpty = (value: string | Array<any>) => {
+  return value.length === 0;
+};
+
+export default isEmpty;
---
+++ src/utils/move-to-page.ts
@@ -0,0 +1,5 @@
+const movetoResultPage = (path: string) => {
+  window.location.href = `https://clinicaltrialskorea.com/studies?conditions=${path}`;
+};
+
+export default movetoResultPage;
