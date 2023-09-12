697fdd26e32160d58e304440d01156c6ddb18ddc
hwinkr <dnddl8280@naver.com>
2023-09-12 16:24:37 +0900

    Chore: style import 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
-import GlobalStyleProvider from './types/GlobalStyleProvider';
+import GlobalStyleProvider from './styles/GlobalStyleProvider';
 
 function App() {
   return (
